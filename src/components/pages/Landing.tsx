ccc2f9f60c5c3faf6041abc694ef8a678fc36137
SuzanaRistic <suzi_hanzi@hotmail.com>
2021-08-24 16:48:16 +0200

    startsida klar

--- src/components/pages/Landing.tsx
+++ src/components/pages/Landing.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './../../styles/Landing.scss'
-
+import { Link } from 'react-router-dom'
 import Header from '../Header'
 
 function Landing() {
@@ -12,8 +12,16 @@ function Landing() {
         
         <div className="white-container-wrap">
         <div className="white-container">
-            
+        <div className="button-container">
+        <button className="meny-button">
+        <Link to="/meny">Meny</Link><br></br>
+        </button><br></br>
+        <div><hr></hr></div>
+        <button className="boka-button">
+        <Link to="/boka">Boka Bord</Link></button>
+        <div className="picture-container"></div>
 
+        </div>
              </div>  
         </div>
         </>
