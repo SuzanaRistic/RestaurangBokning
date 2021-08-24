3dbdaa9174e707c0567b9137b4e7b2c87dbead47
ballardinimarta <marta.ballardini@gmail.com>
2021-08-24 11:28:48 +0200

    Header with props and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { Route,BrowserRouter as Router, Switch } from 'react-router-dom';
+import Booking from './components/pages/Booking';
+import Landing from './components/pages/Landing';
+import Menu from './components/pages/Menu';
 import './styles/App.scss'
 
 
 function App() {
 
   return (
-    <div>
-        Hej
-        <img src='./images/lazy-logo-white.svg' alt="" />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Landing/>
+        </Route>
+        <Route path="/meny">
+          <Menu/>
+        </Route>
+        <Route path="/boka">
+          <Booking/>
+        </Route>
+      </Switch>
+    </Router>
+    
   );
 }
 
