688c2a73482ece9c942de94e12c72e47950a52c0
ballardinimarta <marta.ballardini@gmail.com>
2021-08-24 13:19:02 +0200

    scss fixed

--- src/components/pages/Booking.tsx
+++ src/components/pages/Booking.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import BookingForm from '../BookingForm'
 import Header from '../Header'
 
 function Booking() {
     return (
         <div>
             <Header title="Boka"></Header>
+            <BookingForm></BookingForm>
         </div>
     )
 }
