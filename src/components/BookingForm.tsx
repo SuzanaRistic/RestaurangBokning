5f405c250c8de499ee9f18ae32128bcf123dfdab
Melina Kamyab <melina.kamyab@gmail.com>
2021-08-26 13:34:57 +0200

    create conditionals

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -22,7 +22,7 @@ function BookingForm() {
         time: '',
         date: ''
     })
-    
+
     const [requestedBooking, setRequestedBooking] = useState({
         guests: 1,
         date: ''
@@ -39,21 +39,21 @@ function BookingForm() {
     const phoneRef = createRef<HTMLInputElement>();
     const emailRef = createRef<HTMLInputElement>();
     const messageRef = createRef<HTMLTextAreaElement>();
-    
 
 
 
-     const booking_ref = uuidv4();
-   
-    function sendFirstPart () {
+
+    const booking_ref = uuidv4();
+
+    function sendFirstPart() {
         setFirstPart({
             guests: Number(guestsRef.current?.value),
-            date:  (dateRef.current?.value)?.toString() || '2021-09-29',
+            date: (dateRef.current?.value)?.toString() || '2021-09-29',
             time: time
         })
         setShowFirst(false)
     }
-    function SendBooking () {
+    function SendBooking() {
         setBooking({
             guests: firstPart.guests,
             firstname: firstNameRef.current?.value || ' ',
@@ -62,23 +62,23 @@ function BookingForm() {
             phonenumber: phoneRef.current?.value || ' ',
             time: firstPart.time,
             booking_reference: booking_ref,
-            date:  firstPart.date,
+            date: firstPart.date,
             message: messageRef.current?.value || ' '
         })
-        
+
     }
     useEffect(() => {
         if (booking) {
-            axios.post('http://localhost:4000/bookings', booking )
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+            axios.post('http://localhost:4000/bookings', booking)
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
             history.push(`/bokningsbekraftelse/${booking.booking_reference}`)
-        } 
-        
+        }
+
     }, [booking])
 
     //store requested date and number of guests in variables 
@@ -97,93 +97,92 @@ function BookingForm() {
                 setBookingList(res.data)
                 const totalNumberOfGuestsList = bookingList?.filter(totalGuests => (totalGuests.date === requestedBooking?.date)).map(filteredGuests => (filteredGuests.guests));
                 const totalNumberOfGuestsForRequestedDate = totalNumberOfGuestsList?.reduce((a, b) => a + b, 0);
-                setGuests({ guestsForRequestedDate: Number(totalNumberOfGuestsForRequestedDate), guestsTOne: 0, guestsTTwo: 0 })
+                setGuests({ guestsForRequestedDate: totalNumberOfGuestsForRequestedDate || 0, guestsTOne: 0, guestsTTwo: 0 })
                 console.log("total number of guests that have already booked: ", totalNumberOfGuestsForRequestedDate);
             }).catch((error) => {
                 console.log(error)
             });
     }
-   
 
     return (
         <>
-        { showFirst &&  
-        <div className="white-container-booking" >
+            {showFirst &&
+                <div className="white-container-booking" >
 
-            <label htmlFor="guests">Antal Gäster</label>
-            <select name="guests" id="guests" ref={guestsRef} required>
-                <option value={1} >1</option>
-                <option value={2} >2</option>
-                <option value={3} >3</option>
-                <option value={4} >4</option>
-                <option value={5} >5</option>
-                <option value={6} >6</option>
-                <option value={7} >7</option>
-                <option value={8} >8</option>
-                <option value={9} >9</option>
-                <option value={10} >10</option>
-                <option value={11} >11</option>
-                <option value={12} >12</option>
-            </select>
-            <div className="date-time-wrap">
-                <div>
-                    <label htmlFor="date">Datum</label>
-                    <input ref={dateRef} type="date" name="date" id="date" required/>
-                    {requestedBooking.guests > 90}{<h2>Sorry, we are fully booked today</h2>}
+                    <label htmlFor="guests">Antal Gäster</label>
+                    <select name="guests" id="guests" ref={guestsRef} required>
+                        <option value={1} >1</option>
+                        <option value={2} >2</option>
+                        <option value={3} >3</option>
+                        <option value={4} >4</option>
+                        <option value={5} >5</option>
+                        <option value={6} >6</option>
+                        <option value={7} >7</option>
+                        <option value={8} >8</option>
+                        <option value={9} >9</option>
+                        <option value={10} >10</option>
+                        <option value={11} >11</option>
+                        <option value={12} >12</option>
+                    </select>
+                    <div className="date-time-wrap">
+                        <div>
+                            <label htmlFor="date">Datum</label>
+                            <input ref={dateRef} type="date" name="date" id="date" required onChange={sendRequest} />
+                            {guests.guestsForRequestedDate + requestedBooking.guests  >10 ? <p>Sorry, we are fully booked today</p> : <p>Welcome</p>}
+
+                        </div>
+                        <div>
+                            <label htmlFor="tid">Tid</label>
+                            {toggleTimeBtns &&
+                                <div className="time-btns">
+
+                                    <button className="time-btn-clicked" >18:00</button>
+                                    <button className="time-btn" onClick={() => { setToggleTimeBtns(false); setTime('21:00') }} >21:00</button>
+                                </div>
+                            }
+                            {!toggleTimeBtns &&
+                                <div className="time-btns">
+                                    <button className="time-btn" onClick={() => { setToggleTimeBtns(true); setTime('18:00') }} > 18:00</button>
+                                    <button className="time-btn-clicked"  >21:00</button>
+                                </div>
+                            }
+
+                        </div>
+                    </div>
+                    <button className="confirm-btn" onClick={(e) => { e.preventDefault(); sendFirstPart(); }}>
+                        <img src={gavidare} alt="" />
+                    </button>
+                </div>}
+            {!showFirst &&
+                <div className="white-container-booking">
+                    <div className="booking-info-container">
+                        <p>Antal: <br></br> {firstPart.guests}</p>
+                        <p>Datum: <br></br> {firstPart.date}</p>
+                        <p>Tid: <br></br> {firstPart.time}</p>
+
+                    </div>
+                    <label htmlFor="firstname">Förnamn</label>
+                    <input type="text" name="firstname" ref={firstNameRef} required />
+
+                    <label htmlFor="lastname">Efternamn</label>
+                    <input type="text" name="lastname" ref={lastNameRef} required />
+
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" ref={emailRef} required />
+
+                    <label htmlFor="telefon">Telefon</label>
+                    <input type="tel" name="telefon" ref={phoneRef} required />
+
+                    <label htmlFor="message">Allergener/Önskemål</label>
+                    <textarea rows={4} name="message" ref={messageRef} required />
+
+                    <button className="confirm-btn" onClick={SendBooking}>
+                        <img src={button} alt="" />
+                    </button>
 
                 </div>
-                <div>
-                    <label htmlFor="tid">Tid</label>
-                    {toggleTimeBtns && 
-                    <div className="time-btns">
-                    
-                    <button className="time-btn-clicked" >18:00</button>
-                    <button className="time-btn" onClick={()=>{setToggleTimeBtns(false); setTime('21:00')}} >21:00</button>
-                    </div>
-                    }
-                    {!toggleTimeBtns && 
-                    <div className="time-btns">
-                    <button className="time-btn" onClick={()=>{setToggleTimeBtns(true); setTime('18:00')}} > 18:00</button>
-                    <button  className="time-btn-clicked"  >21:00</button>
-                    </div>
-                    }
-                    
-                </div>
-            </div>
-            <button className="confirm-btn" onClick={(e)=>{e.preventDefault(); sendFirstPart(); sendTime(); }}>
-                <img src={gavidare} alt="" />
-            </button> 
-        </div> }
-        {!showFirst && 
-         <div className="white-container-booking">
-             <div className="booking-info-container">
-                 <p>Antal: <br></br> {firstPart.guests}</p>
-                 <p>Datum: <br></br> {firstPart.date}</p>
-                 <p>Tid: <br></br> {firstPart.time}</p>
 
-             </div>
-         <label htmlFor="firstname">Förnamn</label>
-                 <input type="text" name="firstname" ref={firstNameRef} required/>
- 
-                 <label htmlFor="lastname">Efternamn</label>
-                 <input type="text" name="lastname" ref={lastNameRef} required/>
- 
-                 <label htmlFor="email">Email</label>
-                 <input type="email" name="email" ref={emailRef} required/>
- 
-                 <label htmlFor="telefon">Telefon</label>
-                 <input type="tel" name="telefon" ref={phoneRef} required/>
- 
-                 <label htmlFor="message">Allergener/Önskemål</label>
-                 <textarea rows={4} name="message" ref={messageRef} required/>
-
-                <button className="confirm-btn" onClick={SendBooking}>
-                    <img src={button} alt=""  />
-                </button> 
-                
-         </div>
-     
-        }
+            }
         </>
 
     )
