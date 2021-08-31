9e2335c5687c13818742a5969f69a389fcb22d53
SuzanaRistic <suzi_hanzi@hotmail.com>
2021-08-31 10:40:36 +0200

    test

--- src/components/pages/Confirmation.tsx
+++ src/components/pages/Confirmation.tsx
@@ -1,33 +1,36 @@
-import axios from 'axios';
-import React, { useState } from 'react'
-import { useHistory, useParams } from 'react-router-dom';
-import Header from '../Header'
-import './../../styles/Confirmation.scss'
-import logo from './../../images/logo-with-background.svg'
+import axios from "axios";
+import React, {useState} from "react";
+import {useHistory, useParams} from "react-router-dom";
+import Header from "../Header";
+import "./../../styles/Confirmation.scss";
+import logo from "./../../images/logo-with-background.svg";
 export interface IParams {
-	ref: string;
+  ref: string;
 }
 function Confirmation() {
-    const history = useHistory();
-    const { ref } = useParams<IParams>();
-    return (
-        <>
-            <Header title="Tack!"></Header>
-            <div className="white-container-wrapper">
-                <div className="white-container" style={{height: '80vh'}}>
-                    <p>Tack så mycket för din bokning hos The Lazy Bee!</p>
-                    <p>Din bokningsreferens är: {ref}</p>
-                    <p>Välkommen!</p>
-                    <img src={logo} alt="logo"></img>
-                    <button onClick={() => {history.push(`/avboka/${ref}`)}}> Avboka Här!</button>
-
-                </div>
-            </div>
-           
-            
-            
-        </>
-    )
+  const history = useHistory();
+  const {ref} = useParams<IParams>();
+  return (
+    <>
+      <Header title="Tack!"></Header>
+      <div className="white-container-wrapper">
+        <div className="white-container" style={{height: "80vh"}}>
+          <p>Tack så mycket för din bokning hos The Lazy Bee!</p>
+          <p>Din bokningsreferens är: {ref}</p>
+          <p>Välkommen!</p>
+          <img src={logo} alt="logo"></img>
+          <button
+            onClick={() => {
+              history.push(`/avboka/${ref}`);
+            }}
+          >
+            {" "}
+            Avboka Här!
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Confirmation
+export default Confirmation;
--- src/components/pages/Menu.tsx
+++ src/components/pages/Menu.tsx
@@ -1,20 +1,101 @@
-import React from 'react'
-import Header from '../Header'
-import bookbutton from './../../images/bokaknapp.png'
-import decoflower from './../../images/bulletpoint 1.png'
-import './../../styles/Menu.scss'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Header from "../Header";
+import bookbutton from "./../../images/bokaknapp.png";
+import decoflower from "./../../images/bulletpoint 1.png";
+import "./../../styles/Menu.scss";
+import {Link} from "react-router-dom";
 
 function Menu() {
-    return (
-        <>
-            <Header title="Meny"></Header>
-        <div className="white-container-wrap">
-        <div className="white-container"> 
-        <div className="food-container">
-        <h3 className="food-name"><img className="decoflower" src={decoflower} alt=""/> Avocadomacka</h3>
-        <p className="food-description">Babyplommon tomater, avocado och parmaskinka på smörstekt krispig levain bröd</p>
+  return (
+    <>
+      <Header title="Meny"></Header>
+      <div className="white-container-wrap">
+        <div className="white-container">
+          <div className="food-container">
+            <h3 className="food-name">
+              <img className="decoflower" src={decoflower} alt="" />{" "}
+              Avocadomacka
+            </h3>
+            <p className="food-description">
+              Plommontomater, avocado och prosciutto på smörstekt krispigt
+              levainbröd
+            </p>
+          </div>
+          <hr></hr>
+          <div className="food-container">
+            <h3 className="food-name">
+              <img className="decoflower" src={decoflower} alt="" /> Eggs
+              Benedict
+            </h3>
+            <p className="food-description">
+              Pocherat ägg med hollandaisesås, färskost och avocado på grillat bröd
+            </p>
+          </div>
+          <hr></hr>
+          <div className="food-container">
+            <h3 className="food-name">
+              <img className="decoflower" src={decoflower} alt="" /> Lazy Bee
+              burger
+            </h3>
+            <p className="food-description">
+              Sesamtoppat surdegsbröd med sallad, tomat, picklad rödlök, krispig
+              bacon och vår fantastiska Lazy Bee-sås
+            </p>
+          </div>
+          <hr></hr>
+          <div className="food-container">
+            <h3 className="food-name">
+              <img className="decoflower" src={decoflower} alt="" /> Pannkakor
+            </h3>
+            <p className="food-description">
+              •Välj mellan: Amercan pancakes, Crêpes eller Belgiska våfflor <br></br>
+              •...och 3 toppings: Oreo-crunch, jordnötssmör, Ekologisk
+              marmelad med kex, sylt (jorgubbs- eller äppelsylt), färsk frukttopping
+            </p>
+          </div>
+          <hr></hr>
+          <div className="food-container">
+            <h3 className="food-name">
+              <img className="decoflower" src={decoflower} alt="" />{" "}
+              Rainbow-Choc Bowl{" "}
+            </h3>
+            <p className="food-description">
+              Havregryn med kakao, jordnötssmör och chokladbitar, toppad med
+              färska jordgubbar, blåbär, hallon, nötter och kokosflingor
+            </p>
+          </div>
+          <hr></hr>
+          <div className="food-container">
+            <h3 className="food-name">
+              <img className="decoflower" src={decoflower} alt="" /> Fruktbowl
+            </h3>
+            <p className="food-description">
+              •Tropisk bowl (ananas, banan, papaya, mango toppad med vaniljkräm,
+              passionfruktspuré och kokosflingor)<br></br>
+              •Berry bowl (jorgubbar, blåbär, hallon, köksbär, björnbär, tranbär
+              toppad med grädde, chokladflingor och nötter)<br></br>
+              •Acai bowl (musli-crunch, frukt, kokos, jordnötssmör)
+            </p>
+          </div>
+          <hr></hr>
+          <div className="food-container">
+            <h3 className="food-name">
+              <img className="decoflower" src={decoflower} alt="" /> Grillad
+              macka{" "}
+            </h3>
+            <p className="food-description">
+              Grillat italienskt bröd med smör, tomat, mozzarela och pesto
+              <br></br>
+              •Välj protein: kyckling, lax, tonfisk, grillade grönsaker
+            </p>
+          </div>
+          <Link to="/boka">
+            <button className="book-btn">
+              <img src={bookbutton} alt="" />
+            </button>
+          </Link>
         </div>
+<<<<<<< HEAD
         <hr></hr>
         <div className="food-container">
         <h3 className="food-name"><img className="decoflower" src={decoflower} alt=""/> Eggs Benedict</h3>
@@ -57,6 +138,11 @@ function Menu() {
 
         </>
     )
+=======
+      </div>
+    </>
+  );
+>>>>>>> bbc6dfe97f082941aa58f611108f02f47050bb7b
 }
 
-export default Menu
+export default Menu;
