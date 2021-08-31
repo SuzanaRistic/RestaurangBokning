cf11a2173a276cf6547353324165237057a9f242
Melina Kamyab <melina.kamyab@gmail.com>
2021-08-31 15:20:06 +0200

    confirm email WIP

--- src/components/GuestForm.tsx
+++ src/components/GuestForm.tsx
@@ -1,15 +1,15 @@
-import {useFormik} from "formik";
-import * as yup from "yup";
-import "./../styles/Booking.scss";
-import {createRef, useEffect, useState} from "react";
-import button from "./../images/bekfräftaknapp.svg";
-import IBooking from "../interfaces/IBooking";
-import {v4 as uuidv4} from "uuid";
-import axios from "axios";
-import {useHistory} from "react-router-dom";
-import "yup-phone";
-import {init, send} from "emailjs-com";
-init("user_OHyxXpSu4H1rj4LKQ2q7L");
+import {useFormik} from 'formik';
+import * as yup from 'yup';
+import './../styles/Booking.scss';
+import {createRef, useEffect, useState} from 'react';
+import button from './../images/bekfräftaknapp.svg';
+import IBooking from '../interfaces/IBooking';
+import {v4 as uuidv4} from 'uuid';
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
+import 'yup-phone';
+import {init, send} from 'emailjs-com';
+init('user_OHyxXpSu4H1rj4LKQ2q7L');
 
 interface IGuestComponentProps {
   time: string;
@@ -32,34 +32,34 @@ const GuestComponent = (props: IGuestComponentProps) => {
     /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/;
   const formik = useFormik({
     initialValues: {
-      firstname: "",
-      lastname: "",
-      email: "",
-      phonenumber: "",
-      message: "",
+      firstname: '',
+      lastname: '',
+      email: '',
+      phonenumber: '',
+      message: '',
     },
     validationSchema: yup.object({
       firstname: yup
         .string()
-        .max(15, "Firstname should not exceed 15 characters.")
-        .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed in this field.")
-        .required("Please enter your firstname."),
+        .max(15, 'Firstname should not exceed 15 characters.')
+        .matches(/^[aA-zZ\s]+$/, 'Only alphabets are allowed in this field.')
+        .required('Please enter your firstname.'),
       lastname: yup
         .string()
-        .max(15, "Lastname should not exceed 15 characters.")
-        .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed in this field.")
-        .required("Please enter your lastname."),
+        .max(15, 'Lastname should not exceed 15 characters.')
+        .matches(/^[aA-zZ\s]+$/, 'Only alphabets are allowed in this field.')
+        .required('Please enter your lastname.'),
       email: yup
         .string()
-        .email("Invalid email address.")
-        .required("Please enter your email."),
+        .email('Invalid email address.')
+        .required('Please enter your email.'),
       phonenumber: yup
         .string()
-        .matches(phoneValid, "Invalid phone number.")
-        .required("Please enter your phone number"),
+        .matches(phoneValid, 'Invalid phone number.')
+        .required('Please enter your phone number'),
       message: yup
         .string()
-        .max(150, "Message should not exceed 150 characters."),
+        .max(150, 'Message should not exceed 150 characters.'),
     }),
     onSubmit: (values) => {
       alert(JSON.stringify(values));
@@ -69,14 +69,14 @@ const GuestComponent = (props: IGuestComponentProps) => {
   function SendBooking() {
     setBooking({
       guests: props.guests,
-      firstname: firstNameRef.current?.value || " ",
-      lastname: lastNameRef.current?.value || " ",
-      email: emailRef.current?.value || " ",
-      phonenumber: phoneRef.current?.value || " ",
+      firstname: firstNameRef.current?.value || ' ',
+      lastname: lastNameRef.current?.value || ' ',
+      email: emailRef.current?.value || ' ',
+      phonenumber: phoneRef.current?.value || ' ',
       time: props.time,
       booking_reference: booking_ref,
       date: props.date,
-      message: messageRef.current?.value || " ",
+      message: messageRef.current?.value || ' ',
     });
     var templateParams = {
       firstname: firstNameRef.current?.value,
@@ -85,20 +85,22 @@ const GuestComponent = (props: IGuestComponentProps) => {
       to_email: emailRef.current?.value,
       bookingref: booking_ref,
       cancelBooking: `http://localhost:4000/avboka/${booking_ref}`,
-      guests: props.guests
+      guests: props.guests,
     };
-    send('service_1t779ze', 'template_ui3c5na', templateParams)
-    .then(function(response) {
+    send('service_1t779ze', 'template_ui3c5na', templateParams).then(
+      function (response) {
         console.log('SUCCESS!', response.status, response.text);
-        }, function(error) {
+      },
+      function (error) {
         console.log('FAILED...', error);
-      })
+      }
+    );
   }
 
   useEffect(() => {
     if (booking) {
       axios
-        .post("http://localhost:4000/bookings", booking)
+        .post('http://localhost:4000/bookings', booking)
         .then(function (response) {
           console.log(response);
         })
@@ -114,64 +116,64 @@ const GuestComponent = (props: IGuestComponentProps) => {
       <label htmlFor="firstname">Förnamn*</label>
       <input
         type="text"
-        {...formik.getFieldProps("firstname")}
+        {...formik.getFieldProps('firstname')}
         ref={firstNameRef}
         required
       />
       <div className="invalid-input">
         {formik.touched.firstname && formik.errors.firstname ? (
-          <span style={{color: "red"}}>{formik.errors.firstname}</span>
+          <span style={{color: 'red'}}>{formik.errors.firstname}</span>
         ) : null}
       </div>
 
       <label htmlFor="lastname">Efternamn*</label>
       <input
         type="text"
-        {...formik.getFieldProps("lastname")}
+        {...formik.getFieldProps('lastname')}
         ref={lastNameRef}
         required
       />
       <div className="invalid-input">
         {formik.touched.lastname && formik.errors.lastname ? (
-          <span style={{color: "red"}}>{formik.errors.lastname}</span>
+          <span style={{color: 'red'}}>{formik.errors.lastname}</span>
         ) : null}
       </div>
 
       <label htmlFor="email">Email*</label>
       <input
         type="email"
-        {...formik.getFieldProps("email")}
+        {...formik.getFieldProps('email')}
         ref={emailRef}
         required
       />
       <div className="invalid-input">
         {formik.touched.email && formik.errors.email ? (
-          <span style={{color: "red"}}>{formik.errors.email}</span>
+          <span style={{color: 'red'}}>{formik.errors.email}</span>
         ) : null}
       </div>
 
       <label htmlFor="phonenumber">Telefon*</label>
       <input
         type="tel"
-        {...formik.getFieldProps("phonenumber")}
+        {...formik.getFieldProps('phonenumber')}
         ref={phoneRef}
         required
       />
       <div className="invalid-input">
         {formik.touched.phonenumber && formik.errors.phonenumber ? (
-          <span style={{color: "red"}}>{formik.errors.phonenumber}</span>
+          <span style={{color: 'red'}}>{formik.errors.phonenumber}</span>
         ) : null}
       </div>
 
       <label htmlFor="message">Allergener/Önskemål</label>
       <textarea
         rows={4}
-        {...formik.getFieldProps("message")}
+        {...formik.getFieldProps('message')}
         ref={messageRef}
       />
       <div className="invalid-input">
         {formik.touched.message && formik.errors.message ? (
-          <span style={{color: "red"}}>{formik.errors.message}</span>
+          <span style={{color: 'red'}}>{formik.errors.message}</span>
         ) : null}
       </div>
 
