ae290d86c1ba65172d78e4aecb7b1d21f1f70115
Melina Kamyab <melina.kamyab@gmail.com>
2021-09-01 16:31:32 +0200

    delete GetBooking

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {FindBookings} from '../components/GetBookings';
 
 const mockResponse = {
   data: {
@@ -8,11 +7,11 @@ const mockResponse = {
         guests: 2,
         firstname: 'Luke',
         lastname: 'skywalker',
-        email: 'starwarsRules@gmail.com',
-        phonenumber: '08 763536278',
-        time: '18.00',
-        booking_reference: '98w9058094',
-        date: 2021 - 09 - 24,
+        email: 'starWarsRules@gmail.com',
+        phonenumber: '0733925118',
+        time: '18:00',
+        booking_reference: '83fc878c-4b3d-435d-a708-38a1fecba875',
+        date: "2021 - 09 - 24",
         message: '',
       },
     ],
--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -5,7 +5,6 @@ import IBooking from '../interfaces/IBooking';
 import axios from 'axios';
 import GuestComponent from './GuestForm';
 import {findTables} from './findTime';
-import {FindBookings} from './GetBookings';
 
 function BookingForm() {
   const now = new Date(Date.now());
@@ -43,7 +42,15 @@ function BookingForm() {
   }
 
   useEffect(() => {
-    const bookingsFromDB = FindBookings;
+    axios
+      .get('http://localhost:4000/bookings')
+      .then((res) => {
+        console.log(res.data);
+        setBookingList(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   function sendRequest() {
